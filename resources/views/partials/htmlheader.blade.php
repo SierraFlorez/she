df6036a6952257608e434ff99cbd17e10ad34085
Camilo Sierra <jcsierra247@misena.edu.co>
2020-05-06 20:42:23 -0500

    Lo encontre :D, y elimino el materialize y creo que queda terminado

--- resources/views/partials/htmlheader.blade.php
+++ resources/views/partials/htmlheader.blade.php
@@ -6,39 +6,37 @@
     <meta name="author" content="ThemeSelect">
     <title> SHE - @yield('htmlheader_title', ' ') </title>
     <meta name="description" content="Gestor de Usuarios">
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css')}}">
-    <link href="{{ asset('assets/fonts/circular-std/style.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('libs/css/style.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
-    {{-- DATEPICKER --}}
-    <link rel="stylesheet" href="{{ asset('css/picker.css')}}">
-    <script src="{{ asset('js/picker.js') }}" type="text/javascript"></script>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css')}}">
+     <link href="{{ asset('assets/fonts/circular-std/style.css')}}" rel="stylesheet">
+     <link rel="stylesheet" href="{{ asset('libs/css/style.css')}}">
+     <link rel="stylesheet" href="{{ asset('css/animate.css')}}">
+     {{-- DATEPICKER --}}
+     <link rel="stylesheet" href="{{ asset('css/picker.css')}}">
+     <script src="{{ asset('js/picker.js') }}" type="text/javascript"></script>
 
-    <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome/css/fontawesome-all.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/charts/chartist-bundle/chartist.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/charts/morris-bundle/morris.css')}}">
-    <link rel="stylesheet"
-        href="{{ asset('assets/fonts/material-design-iconic-font/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/charts/c3charts/c3.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/fonts/flag-icon-css/flag-icon.min.css')}}">
-    {{-- DataTables --}}
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('materialize/css/materialize.min.css') }}" /> --}}
-    {{-- Material --}}
-    <link rel="stylesheet" href="{{ asset('assets/fonts/flag-icon-css/flag-icon.min.css')}}">
+     <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome/css/fontawesome-all.css')}}">
+     <link rel="stylesheet" href="{{ asset('assets/charts/chartist-bundle/chartist.css')}}">
+     <link rel="stylesheet" href="{{ asset('assets/charts/morris-bundle/morris.css')}}">
+     <link rel="stylesheet" href="{{ asset('assets/fonts/material-design-iconic-font/css/materialdesignicons.min.css')}}">
+     <link rel="stylesheet" href="{{ asset('assets/charts/c3charts/c3.css')}}">
+     <link rel="stylesheet" href="{{ asset('assets/fonts/flag-icon-css/flag-icon.min.css')}}">
+     {{-- DataTables --}}
+     <link rel="stylesheet" type="text/css" href="{{ asset('DataTables/datatables.min.css') }}"/>
+
 
     {{--Swit Alert --}}
     <script src="{{ asset('js/sweetalert2.min.js') }}" type="text/javascript"></script>
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/sweetalert2.min.css') }}" />
-
-
-
-    <link rel="stylesheet" href="{{ asset('assets/multi-select/css/multi-select.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/datatables/css/dataTables.bootstrap4.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/datatables/css/buttons.bootstrap4.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/datatables/css/select.bootstrap4.css')}}">
-    <link rel="stylesheet" href="{{ asset('assets/datatables/css/fixedHeader.bootstrap4.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/sweetalert2.min.css') }}"/>
+        
+     
+ 
+    <link rel="stylesheet"  href="{{ asset('assets/multi-select/css/multi-select.css')}}">
+    <link rel="stylesheet"  href="{{ asset('assets/datatables/css/dataTables.bootstrap4.css')}}">
+    <link rel="stylesheet"  href="{{ asset('assets/datatables/css/buttons.bootstrap4.css')}}">
+    <link rel="stylesheet"  href="{{ asset('assets/datatables/css/select.bootstrap4.css')}}">
+    <link rel="stylesheet"  href="{{ asset('assets/datatables/css/fixedHeader.bootstrap4.css')}}">
 </head>
